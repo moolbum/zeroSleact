9827a7fee74b0059531d5b13c38c0bb13df3517b
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2022-04-12 14:59:29 +0900

    Add :초기세팅 완료

---
+++ alecture/layouts/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+const Login = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+//코드 스플리팅의 단위는 페이지 단위로
+// SSR 안하는 페이지
+
+const App = () => {
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
+};
+
+export default App;
